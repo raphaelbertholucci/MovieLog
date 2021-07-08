70b862397bb50b20dd7ba2b8ff052051eba74026
Raphael Bertholucci <raphaelbertholucci@gmail.com>
2021-07-07 22:22:21 -0300

    change layout

---
+++ app/src/main/kotlin/com/bertholucci/movielog/FavoritesFragment.kt
@@ -0,0 +1,7 @@
+package com.bertholucci.movielog
+
+import com.bertholucci.core.base.BaseFragment
+import com.bertholucci.movielog.databinding.FragmentHomeBinding
+
+class FavoritesFragment: BaseFragment<FragmentHomeBinding>(R.layout.fragment_home) {
+}
---
+++ app/src/main/kotlin/com/bertholucci/movielog/HomeFragment.kt
@@ -0,0 +1,7 @@
+package com.bertholucci.movielog
+
+import com.bertholucci.core.base.BaseFragment
+import com.bertholucci.movielog.databinding.FragmentHomeBinding
+
+class HomeFragment: BaseFragment<FragmentHomeBinding>(R.layout.fragment_home) {
+}
