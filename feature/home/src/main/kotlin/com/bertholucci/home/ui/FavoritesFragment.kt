503804c07bd7b6b1e24b09183a7483724f463649
Raphael Bertholucci <raphaelbertholucci@gmail.com>
2021-07-12 21:13:44 -0300

    home module removed

--- feature/home/src/main/kotlin/com/bertholucci/home/ui/FavoritesFragment.kt
+++
@@ -1,8 +0,0 @@
-package com.bertholucci.home.ui
-
-import com.bertholucci.core.base.BaseFragment
-import com.bertholucci.home.R
-import com.bertholucci.home.databinding.FragmentFavoritesBinding
-
-class FavoritesFragment : BaseFragment<FragmentFavoritesBinding>(R.layout.fragment_favorites) {
-}
--- feature/movie/src/main/kotlin/com/bertholucci/movie/MovieInitialization.kt
+++
@@ -1,8 +0,0 @@
-package com.bertholucci.movie
-
-import org.koin.android.viewmodel.dsl.viewModel
-import org.koin.dsl.module
-
-val movieModule = module {
-    viewModel { MovieViewModel(repository = get()) }
-}
