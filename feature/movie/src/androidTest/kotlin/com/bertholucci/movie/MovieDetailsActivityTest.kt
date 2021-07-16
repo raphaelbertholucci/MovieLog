c743dfcd03726e51ad9be43c0ba13fe7034075c3
Raphael Bertholucci <raphaelbertholucci@gmail.com>
2021-07-15 23:18:28 -0300

    add dependencyUpdates plugin and fix intrumented tests execution

--- feature/movie/src/androidTest/kotlin/com/bertholucci/movie/MovieDetailsActivityTest.kt
+++ feature/movie/src/androidTest/kotlin/com/bertholucci/movie/MovieDetailsActivityTest.kt
@@ -1,11 +1,18 @@
 package com.bertholucci.movie
 
 import androidx.test.ext.junit.runners.AndroidJUnit4
+import org.junit.After
 import org.junit.Test
 import org.junit.runner.RunWith
+import org.koin.core.context.stopKoin
 
 @RunWith(AndroidJUnit4::class)
 class MovieDetailsActivityTest {
+
+    @After
+    fun tearDown() {
+        stopKoin()
+    }
 
     @Test
     fun showCorrectTexts() {
